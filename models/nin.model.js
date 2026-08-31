fa30937dd116a1da6ef94c9df955cda84025778d
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-08-31 12:43:06 +0100

    Added NIN model

---
+++ models/nin.model.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const ninSchema = new mongoose.Schema(
+  {
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+      required: true,
+    },
+
+    nin: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    firstName: {
+      type: String,
+      required: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+    },
+
+    dob: {
+      type: Date,
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    verificationStatus: {
+      type: String,
+      enum: ["verified", "failed"],
+      required: true,
+    },
+
+    verifiedAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Nin", ninSchema);
