8bb3e4c01808e9a92e1ba1529fb26666b24b8150
Adeshinayomi <adeshinabhadmus5@gmail.com>
2026-09-02 10:30:35 +0100

    Add account balance retrieval functionality: implement route and controller for fetching account balance using NIBSS API

--- controller/account.controller.js
+++ controller/account.controller.js
@@ -1,5 +1,5 @@
 const Account = require("../models/account.model");
-const { getNameEnquiry} = require("../services/nibbs.service");
+const { getNameEnquiry,getAccountBalance} = require("../services/nibbs.service");
 
 const getAccountName = async (req, res) => {
   try {
@@ -7,12 +7,24 @@ const getAccountName = async (req, res) => {
     const result= await getNameEnquiry(accountNumber);
 
     res.status(200).json({ name: result.accountName });
-    
+
   } catch (error) {
     res.status(500).json({ message: 'Error fetching name enquiry.' });
   }
 };
 
+const getBalance = async (req, res) => {
+  try {
+    const { accountNumber } = req.params;
+    const result = await getAccountBalance(accountNumber);
+
+    res.status(200).json({ balance: result.balance });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching account balance.' });
+  }
+};
+
 module.exports = {
   getAccountName,
+  getBalance
 };
--- routes/account.route.js
+++ routes/account.route.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const route = express.Router()
-const {getAccountName}=require('../controller/account.controller')
+const {getAccountName,getBalance}=require('../controller/account.controller')
 const auth=require('../middleware/auth')
 
 route.get('/getAccountName/:accountNumber', auth, getAccountName)
+route.get('/getAccountBalance/:accountNumber', auth, getBalance)
 
 module.exports = route
